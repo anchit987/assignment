c52e17fda199768e11dc1919e0c72fc188fa1bf3
anchit987 <anchit987@gmail.com>
2022-11-03 12:31:47 +0530

    change in index.php

--- index.php
+++ index.php
@@ -10,5 +10,8 @@
 Picture" width="300" height="300"></a>
 <h4>Full Name : Anchit Gangrade</h4>
 <h4>Email : anchit988@gmail.com</h4>
+<h4>College : Shri Govindram Seksaria Institute of Technology and Science Indore</h4>
+<h4>Specialization : Android Development</h4>
+<h4></h4>
 </body>
 </html>
